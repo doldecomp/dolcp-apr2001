6471ff4821da38255343749e5314e916849af7be
RevoSucks <projectrevotpp@hotmail.com>
2024-04-26 23:44:20 -0400

    shaderTCInternals

--- include/shader.h
+++ include/shader.h
@@ -53,6 +53,208 @@ struct SHDRTexCoord {
     /* 0x14 */ u8 texCoordExpIdx;
 };
 
+enum SHADER_TYPE {
+    SHADER_TYPE_TEXTURE = 0,
+    SHADER_TYPE_COLOR = 1,
+    SHADER_TYPE_RASTERIZED = 2,
+    SHADER_TYPE_CONSTANT = 3,
+    SHADER_TYPE_COMPLEXINPUT = 4,
+    SHADER_TYPE_COMPLEX = 5,
+    SHADER_TYPE_EMPTY = 6,
+};
+
+enum SHADER_CHANNEL {
+    SHADER_CHANNEL_RGB = 0,
+    SHADER_CHANNEL_RRR = 1,
+    SHADER_CHANNEL_GGG = 2,
+    SHADER_CHANNEL_BBB = 3,
+    SHADER_CHANNEL_A = 4,
+    SHADER_CHANNEL_TRIVIAL = 5,
+};
+
+enum SHADER_COLOR_TYPE {
+    SHADER_CPREV = 0,
+    SHADER_C0 = 1,
+    SHADER_C1 = 2,
+    SHADER_C2 = 3,
+    SHADER_APREV = 4,
+    SHADER_A0 = 5,
+    SHADER_A1 = 6,
+    SHADER_A2 = 7,
+    SHADER_TEXC = 8,
+    SHADER_TEXA = 9,
+    SHADER_RASC = 10,
+    SHADER_RASA = 11,
+    SHADER_TEXRRR = 12,
+    SHADER_TEXGGG = 13,
+    SHADER_TEXBBB = 14,
+    SHADER_HALF = 15,
+    SHADER_QUARTER = 16,
+    SHADER_COMPLEXINPUT0_RGB = 25,
+    SHADER_COMPLEXINPUT1_RGB = 26,
+    SHADER_COMPLEXINPUT2_RGB = 27,
+    SHADER_COMPLEXINPUT3_RGB = 28,
+    SHADER_COMPLEXINPUT4_RGB = 29,
+    SHADER_COMPLEXINPUT5_RGB = 30,
+    SHADER_COMPLEXINPUT6_RGB = 31,
+    SHADER_COMPLEXINPUT7_RGB = 32,
+    SHADER_COMPLEXINPUT0_A = 35,
+    SHADER_COMPLEXINPUT1_A = 36,
+    SHADER_COMPLEXINPUT2_A = 37,
+    SHADER_COMPLEXINPUT3_A = 38,
+    SHADER_COMPLEXINPUT4_A = 39,
+    SHADER_COMPLEXINPUT5_A = 40,
+    SHADER_COMPLEXINPUT6_A = 41,
+    SHADER_COMPLEXINPUT7_A = 42,
+    SHADER_COLORINPUT0_RGB = 50,
+    SHADER_COLORINPUT1_RGB = 51,
+    SHADER_COLORINPUT2_RGB = 52,
+    SHADER_COLORINPUT3_RGB = 53,
+    SHADER_COLORINPUT0_A = 60,
+    SHADER_COLORINPUT1_A = 61,
+    SHADER_COLORINPUT2_A = 62,
+    SHADER_COLORINPUT3_A = 63,
+    SHADER_NONTRIVIAL = 100,
+    SHADER_ONE = 200,
+    SHADER_ZERO = 201,
+};
+
+enum SHADER_TEX {
+    SHADER_TEX0 = 0,
+    SHADER_TEX1 = 1,
+    SHADER_TEX2 = 2,
+    SHADER_TEX3 = 3,
+    SHADER_TEX4 = 4,
+    SHADER_TEX5 = 5,
+    SHADER_TEX6 = 6,
+    SHADER_TEX7 = 7,
+    SHADER_TEXNONE = 8,
+};
+
+enum SHADER_COLOR {
+    SHADER_COLOR0 = 0,
+    SHADER_COLOR1 = 1,
+    SHADER_COLOR2 = 2,
+    SHADER_COLOR3 = 3,
+    SHADER_COLOR_NONE = 4,
+};
+
+enum SHADER_RAS {
+    SHADER_RAS0 = 0,
+    SHADER_RAS1 = 1,
+    SHADER_RASNONE = 2,
+};
+
+enum SHADER_OP {
+    SHADER_OP_ADD = 0,
+    SHADER_OP_SUB = 1,
+};
+
+enum SHADER_CLAMP {
+    SHADER_CLAMP_LINEAR_1023 = 0,
+    SHADER_CLAMP_LINEAR_255 = 1,
+    SHADER_CLAMP_GE_255 = 2,
+    SHADER_CLAMP_GE_0 = 3,
+    SHADER_CLAMP_EQ_255 = 4,
+    SHADER_CLAMP_EQ_0 = 5,
+    SHADER_CLAMP_LE_255 = 6,
+    SHADER_CLAMP_LE_0 = 7,
+};
+
+enum SHADER_BIAS {
+    SHADER_BIAS_ZERO = 0,
+    SHADER_BIAS_ADDHALF = 1,
+    SHADER_BIAS_SUBHALF = 2,
+};
+
+enum SHADER_SCALE {
+    SHADER_SCALE_1 = 0,
+    SHADER_SCALE_2 = 1,
+    SHADER_SCALE_4 = 2,
+    SHADER_SCALE_DIVIDE_2 = 3,
+};
+
+enum SHADER_COMPLEX {
+    SHADER_COMPLEX0 = 0,
+    SHADER_COMPLEX1 = 1,
+    SHADER_COMPLEX2 = 2,
+    SHADER_COMPLEX3 = 3,
+    SHADER_COMPLEX4 = 4,
+    SHADER_COMPLEX5 = 5,
+    SHADER_COMPLEX6 = 6,
+    SHADER_COMPLEX7 = 7,
+};
+
+struct SHDRShader {
+    /* 0x00 */ enum SHADER_TYPE type;
+    /* 0x04 */ enum SHADER_CHANNEL channel;
+    /* 0x08 */ s16 referenceCount;
+    /* 0x0C */ void *TEVStage;
+    union {
+        struct {
+            /* 0x00 */ enum SHADER_COLOR_TYPE arg;
+        } constantShader;
+        struct {
+            /* 0x00 */ enum SHADER_TEX tex;
+            /* 0x04 */ struct SHDRTexCoord *texCoordShader;
+        } textureShader;
+        struct {
+            /* 0x00 */ enum SHADER_COLOR color;
+        } colorShader;
+        struct {
+            /* 0x00 */ enum SHADER_RAS rasColor;
+        } rasterizedShader;
+        struct {
+            /* 0x00 */ struct SHDRShader * input1;
+            /* 0x04 */ struct SHDRShader * input2;
+            /* 0x08 */ struct SHDRShader * input3;
+            /* 0x0C */ struct SHDRShader * input4;
+            /* 0x10 */ enum SHADER_OP op;
+            /* 0x14 */ enum SHADER_CLAMP clamp;
+            /* 0x18 */ enum SHADER_BIAS bias;
+            /* 0x1C */ enum SHADER_SCALE scale;
+        } complexShader;
+        struct {
+            /* 0x00 */ enum SHADER_COMPLEX input;
+        } complexInputShader;
+    } shaderParams;
+};
+
+struct TexCoordExp {
+    /* 0x00 */ enum SHADER_TG_SRC genSrc;
+    /* 0x04 */ u8 srcShaderIdx;
+    /* 0x08 */ enum SHADER_TG_TYPE type;
+    /* 0x0C */ enum SHADER_MTX mtxInput;
+};
+
+typedef struct {
+    /* 0x00 */ struct TexCoordExp * expressionArray;
+    /* 0x04 */ u8 numExpressions;
+    /* 0x08 */ void * mtxArray[8];
+    /* 0x28 */ u8 mtxUsed[8];
+} SHDRExp;
+
+typedef struct {
+    /* 0x00 */ GXTexCoordID coord[8];
+    /* 0x20 */ struct SHDRTexCoord *tcShader[8];
+    /* 0x40 */ struct SHDRShader *textureShader[8];
+} SHDRResources;
+
+typedef struct {
+    /* 0x00 */ void * colorStages;
+    /* 0x04 */ void * alphaStages;
+    /* 0x08 */ u8 numStages;
+    /* 0x0C */ void * shaderResources;
+    /* 0x10 */ void * texGen;
+} SHDRInfo;
+
+// shaderTCInternals.c
+extern SHDRResources TCResources;
+extern u32 MtxIDArray[8];
+
+void CompileTexGen(struct SHDRShader *shader, SHDRExp **exp);
+void SetTCGenState(SHDRInfo *shader);
+
 // shaderTexCoord.c
 struct SHDRTexCoord * SHDRCreateTexCoordExpression(enum SHADER_TG_SRC src, struct SHDRTexCoord *shadSrc, enum SHADER_TG_TYPE texGenType, enum SHADER_MTX mtxInput);
 struct SHDRTexCoord * SHDRCreateTCPassThrough(enum SHADER_TG_SRC src);
---
+++ src/shader/shaderTCInternals.c
@@ -0,0 +1,143 @@
+#include <dolphin.h>
+#include <dolphin/gx.h>
+#include "shader.h"
+
+SHDRResources TCResources = {
+    // coord[8]
+    {
+        GX_TEXCOORD0,
+        GX_TEXCOORD1,
+        GX_TEXCOORD2,
+        GX_TEXCOORD3,
+        GX_TEXCOORD4,
+        GX_TEXCOORD5,
+        GX_TEXCOORD6,
+        GX_TEXCOORD7
+    },
+    // tcShader[8]
+    {
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+    },
+    // textureShader[8]
+    {
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+    }
+};
+
+u32 MtxIDArray[8] = {
+    0x1E, 0x21, 0x24, 0x27, 0x2A, 0x2D, 0x30, 0x33
+};
+
+static struct SHDRTexCoord * TexCoordArray[8];
+
+static u8 numExpressions;
+
+// functions
+static void FindUniqueTexCoordExpressions(struct SHDRShader *shader);
+static void AddTexCoordShaderToList(struct SHDRTexCoord * shader);
+static void CopyExp(struct SHDRTexCoord *shader, struct TexCoordExp *exp);
+static GXTexMtxType TranslateTexMtxType(enum SHADER_TG_TYPE type);
+
+void CompileTexGen(struct SHDRShader *shader, SHDRExp **exp) {
+    u8 i;
+
+    numExpressions = 0;
+    FindUniqueTexCoordExpressions(shader);
+    *exp = OSAllocFromHeap(__OSCurrHeap, 0x30);
+    if (numExpressions != 0) {
+        (*exp)->expressionArray = OSAllocFromHeap(__OSCurrHeap, numExpressions * 0x10);
+    } else {
+        (*exp)->expressionArray = 0;
+    }
+    (*exp)->numExpressions = numExpressions;
+    for(i = 0; i < 8; i++) {
+        (*exp)->mtxArray[i] = 0;
+        (*exp)->mtxUsed[i] = 0;
+    }
+    for(i = 0; i < numExpressions; i++) {
+        CopyExp(TexCoordArray[i], &(*exp)->expressionArray[i]);
+        if (TexCoordArray[i]->mtxInput != 8 && (*exp)->mtxUsed[TexCoordArray[i]->mtxInput] == 0) {
+            (*exp)->mtxUsed[TexCoordArray[i]->mtxInput] = 1;
+            (*exp)->mtxArray[TexCoordArray[i]->mtxInput] = OSAllocFromHeap(__OSCurrHeap, 0x30);
+        }
+    }
+}
+
+static void FindUniqueTexCoordExpressions(struct SHDRShader *shader) {
+    if (shader) {
+        if (shader->type == SHADER_TYPE_TEXTURE) {
+            AddTexCoordShaderToList(shader->shaderParams.textureShader.texCoordShader);
+            return;
+        }
+        if (shader->type == SHADER_TYPE_COMPLEX || shader->type == SHADER_TYPE_EMPTY) {
+            FindUniqueTexCoordExpressions(shader->shaderParams.complexShader.input1);
+            FindUniqueTexCoordExpressions(shader->shaderParams.complexShader.input2);
+            FindUniqueTexCoordExpressions(shader->shaderParams.complexShader.input3);
+            FindUniqueTexCoordExpressions(shader->shaderParams.complexShader.input4);
+        }
+    }
+}
+
+static void AddTexCoordShaderToList(struct SHDRTexCoord * shader) {
+    u8 i;
+
+    for(i = 0; i < numExpressions; i++) {
+        if (TexCoordArray[i] == shader) {
+            return;
+        }
+    }
+    if (shader->shaderSrc) {
+        AddTexCoordShaderToList(shader->shaderSrc);
+    }
+    ASSERTMSGLINE(0xB3, numExpressions < 8, "Too many texture coordinate shaders - FindUniqueTexCoordExpressions");
+    TexCoordArray[numExpressions] = shader;
+    shader->texCoordExpIdx = numExpressions;
+    numExpressions++;
+}
+
+static void CopyExp(struct SHDRTexCoord *shader, struct TexCoordExp *exp) {
+    exp->genSrc = shader->genSrc;
+    exp->type = shader->type;
+    exp->mtxInput = shader->mtxInput;
+    if (shader->shaderSrc) {
+        exp->srcShaderIdx = shader->shaderSrc->texCoordExpIdx;
+        return;
+    }
+    exp->srcShaderIdx = 0x63;
+}
+
+void SetTCGenState(SHDRInfo *shader) {
+    SHDRExp *tcExp;
+    u8 i;
+    struct TexCoordExp *tcShader;
+    u32 texMtxIdx;
+
+    tcExp = shader->texGen;
+    for(i = 0; i < tcExp->numExpressions; i++) {
+        tcShader = &tcExp->expressionArray[i];
+        if (tcShader->mtxInput == 8) {
+            texMtxIdx = 0x3C;
+        } else {
+            texMtxIdx = MtxIDArray[tcShader->mtxInput];
+            GXLoadTexMtxImm(tcExp->mtxArray[tcShader->mtxInput], texMtxIdx, TranslateTexMtxType(tcShader->type));
+        }
+        if (tcShader->srcShaderIdx != 0x63) {
+            GXSetTexCoordGen(TCResources.coord[i], tcShader->type, tcShader->srcShaderIdx + 0xC, texMtxIdx);
+        } else {
+            GXSetTexCoordGen(TCResources.coord[i], tcShader->type, tcShader->genSrc, texMtxIdx);
+        }
+    }
+    GXSetNumTexGens(tcExp->numExpressions);
+}
+
+static GXTexMtxType TranslateTexMtxType(enum SHADER_TG_TYPE type) {
+    switch(type) {
+        case SHADER_TG_MTX3x4:
+            return GX_MTX3x4;
+        case SHADER_TG_MTX2x4:
+            return GX_MTX2x4;
+        default:
+            ASSERTMSGLINE(0x20C, 0, "unknown tex mtx type in TranslateTexMtxType");
+            return 0;
+    }
+}
